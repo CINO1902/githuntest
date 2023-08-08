6bb7d28a1b57ec907f5b3cf50dc2f7751712f826
ibidapo <ibidapodavid10@gmail.com>
2023-08-08 15:37:49 +0100

    error

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:githubtest/homepage.dart';
+import 'package:githubtest/provider/event_provider.dart';
+import 'package:githubtest/service/event_service.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<ApiService>(
+          create: (_) => ApiService(baseUrl: 'http://jsonplaceholder.typicode.com/todos'),
+        ),
+        ChangeNotifierProxyProvider<ApiService, EventProvider>(
+          create: (_) => EventProvider(apiService: Provider.of<ApiService>(_, listen: false)),
+          update: (_, apiService, eventProvider) => eventProvider!..apiService = apiService,
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      title: 'Event App',
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final eventProvider = Provider.of<EventProvider>(context);
 
-
-
- 
-      
-          
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Event App'),
+      ),
+      body: FutureBuilder(
+        future: eventProvider.fetchAndSetEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Consumer<EventProvider>(
+            builder: (context, eventProvider, child) {
+              final events = eventProvider.events;
+              return ListView.builder(
+                itemCount: events.length,
+                itemBuilder: (context, index) {
+                  final event = events[index];
+                  return ListTile(
+                    title: Text(event.title),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
